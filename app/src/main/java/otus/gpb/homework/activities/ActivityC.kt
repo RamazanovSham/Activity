7b858ddcd5c26004a7397b5e2c9b9e154b307469
Шамиль <ltlmlfs@gmail.com>
2026-03-23 16:41:28 +0300

    dz#1

---
+++ app/src/main/java/otus/gpb/homework/activities/ActivityC.kt
@@ -0,0 +1,44 @@
+package otus.gpb.homework.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ActivityC : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_c)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        findViewById<Button>(R.id.button_c1).setOnClickListener {
+            val intent = Intent(this, ActivityA::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.button_c2).setOnClickListener {
+            val intent = Intent(this, ActivityD::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.button_c3).setOnClickListener {
+            finish()
+        }
+
+        findViewById<Button>(R.id.button_c4).setOnClickListener {
+            val intent = Intent(this, ActivityA::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            finishAffinity()
+        }
+    }
+}
