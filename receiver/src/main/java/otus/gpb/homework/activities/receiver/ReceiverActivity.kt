4f4bf46631d0f16e429aa0bd29cdcb814fcc260a
Шамиль <ltlmlfs@gmail.com>
2026-03-23 17:59:14 +0300

    dz#2

--- receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
+++ receiver/src/main/java/otus/gpb/homework/activities/receiver/ReceiverActivity.kt
@@ -1,6 +1,9 @@
 package otus.gpb.homework.activities.receiver
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ReceiverActivity : AppCompatActivity() {
@@ -8,5 +11,28 @@ class ReceiverActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receiver)
+
+        val titleView: TextView = findViewById(R.id.titleTextView)
+        val yearView: TextView = findViewById(R.id.yearTextView)
+        val descriptionView: TextView = findViewById(R.id.descriptionTextView)
+        val posterView: ImageView = findViewById(R.id.posterImageView)
+
+        val movieTitle = intent.getStringExtra("title") ?: ""
+        val movieYear = intent.getStringExtra("year") ?: ""
+        val movieDescription = intent.getStringExtra("description") ?: ""
+
+        titleView.text = movieTitle
+        yearView.text = movieYear
+        descriptionView.text = movieDescription
+
+        val posterDrawable: Drawable? = when (movieTitle) {
+            "niceguys" -> getDrawable(R.drawable.niceguys)
+            "interstellar" -> getDrawable(R.drawable.interstellar)
+            else -> null
+        }
+
+        posterDrawable?.let {
+            posterView.setImageDrawable(it)
+        }
     }
 }
